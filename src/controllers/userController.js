debca50dcd540c0d079a4c332c646d617ff97bf5
Maira Stefane <dev.maira12@gmail.com>
2025-05-05 13:43:46 -0300

    adicionando database

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,99 @@
 //NESTE ARQUIVO IREMOS PROGRAMAR O QUE SERÁ FEITO AO ACESSAR AQUELA ROTA
+import { PrismaClient } from '@prisma/client';//IMPORTAR O PRISMA CLIENT PARA USAR OS METODOS PRONTOS PARA O CRUD
+const prisma = new PrismaClient();
 
-export const getAllUsers = (req, res) => {
-    res.status(200).json({
-        message: "Rota GET users funcionando!"
-    })
+//RETORNAR TODOS OS USUARIOS
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await prisma.user.findMany()
+        res.status(200).json(users)
+    } catch (error) {
+        res.status(500).json({ message: "erro ao procurar usuarios!" })
+    }
 }
 
-export const postCreateUsers = (req, res) => {
-    const { nome, email } = req.body
-    if (!nome || !email) {
-        res.status(400).json({ erro: "Os dados são obrigatorios!" })
-    } else {
-        const newUsuario = [{
-            nome, email
-        }]
-        res.status(200).json(newUsuario)
+//CRIAR USUARIO
+export const postCreateUsers = async (req, res) => {
+    try {
+        const { name, email } = req.body
+        //se der certo
+        if (!name || !email) {
+            res.status(400).json({ erro: "Os dados são obrigatorios!" })
+        } else {
+            const newUsuario = await prisma.user.create({
+                data: { name, email }
+            })
+            res.status(201).json(newUsuario)
+        }
     }
+    catch (error) {
+        //se der errado
+        res.status(500).json({
+            message: "erro ao criar usuario!",
+            error: error.message
+        })
+    }
+}
+
+//DELETAR USUARIO
+export const deleteUser = async (req, res) => {
+    try {
+        const id = parseInt(req.params.id)
+        await prisma.user.delete({ where: { id } })
+        res.status(200).json({ message: 'delete' })
+    } catch (error) {
+        res.status(500).json({
+            message: "usuario com este id não existe!",
+            error: error.message
+        })
+    }
+}
+
+//ATUALIZAR USUARIO
+export const updateUser = async (req, res) => {
+    const id = parseInt(req.params.id)
+    const { name, email } = req.body
+    try {
+        await prisma.user.update({ where: { id }, data: { name, email } })
+        if (!name && !email) {
+            res.status(400).json({ message: "é necessário atualizar ao menos um campo!" })
+        }
+        res.status(200).json({ message: 'usuario atualizado!' })
+    } catch (error) {
+        res.status(500).json({
+            message: "não foi possivel atualizar usuario!",
+            error: error.message
+        })
+    }
+}
+
+//RETORNAR USUARIO POR ID
+export const getUserId = async (req, res) => {
+    try {
+        const id = parseInt(req.params.id)
+        const users = await prisma.user.findUnique({ where: { id } })
+        if (!users) {
+            return res.status(400).json({ message: "Usuário com este id não existe!" })
+        }
+        res.send(users)
+    }
+    catch (error) {
+        res.json(500).json({
+            message: "erro ao procurar usuario!",
+            error: error.message
+        })
+    }
+}
+
+//RETORNAR POR ID COM CONDIÇÃO(dando erro)
+export const getUserFilterId = async (req, res) => {
+    const letra  = req.body.letra
+    const userers = await prisma.user.findMany({
+        where: {
+            name: {
+                startsWith: letra,
+            },
+        },
+    })
+    res.status(200).json(userers)
 }
