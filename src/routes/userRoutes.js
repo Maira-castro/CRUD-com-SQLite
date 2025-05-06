0f910f60f74fabf0be59c1d3428d4719b21325e9
Maira Stefane <dev.maira12@gmail.com>
2025-05-06 16:08:49 -0300

    add update user validator

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express'
-import { deleteUser, getAllUsers,getUserFilterId,getUserId,postCreateUsers, updateUser } from '../controllers/userController.js'
+import { deleteUser, getAllUsers, getUserFilterId, getUserId, postCreateUsers, updateUser } from '../controllers/userController.js'
+import { validate } from '../middleware/validate.js'
+import { createUserSchema, updateUserSchema } from '../schemas/userSchemas.js'
 const router = express.Router()
 
-router.get("/",getAllUsers)
-router.post("/",postCreateUsers)
-router.delete("/:id",deleteUser)
-router.put("/:id",updateUser)
-router.get("/:id",getUserId)
-router.get("/name/name/",getUserFilterId)
+router.get("/", getAllUsers)
+router.post("/", validate(createUserSchema), postCreateUsers) //a função validate para validar os dados de criação
+//e atribuir o schema correspondete
+router.delete("/:id", deleteUser)
+router.put("/:id", validate(updateUserSchema), updateUser)
+router.get("/:id", getUserId)
+router.get("/name/name/", getUserFilterId)
+
 export default router
